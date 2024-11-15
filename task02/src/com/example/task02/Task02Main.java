34a6d0e68dce3352e44daddd484bf73d54150135
Balandala <balandala05@gmail.com>
2024-11-15 07:47:30 +0500

    task 2 done

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,35 @@
 package com.example.task02;
 
+import javax.sound.midi.Track;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.cert.TrustAnchor;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        int previous = inputStream.read();
+        int current = inputStream.read();
+
+        while (previous != -1){
+
+            if (current == 10 && previous == 13){
+                previous = current;
+                current = inputStream.read();
+            }
+
+                outputStream.write(previous);
+
+            previous = current;
+            current = inputStream.read();
+        }
+        outputStream.flush();
     }
 }
